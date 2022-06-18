b862743d05860c392c1488a45b601b733ab7307a
Vangel Celina Hanna <vangelcelina9@gmail.com>
2022-06-18 12:56:52 +0200

    v1.1

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WD from '@/views/WalkingDead'
+
+const routes = [
+  {path:'/', redirect: '/wd'},
+  {path: '/wd', component: WD}
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
